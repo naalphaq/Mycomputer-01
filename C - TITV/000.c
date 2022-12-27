50934a4ec1bb7c237219d420f6f4e56ab474faf3
Trịnh Nguyễn Anh Quốc <114911301+naalphaq@users.noreply.github.com>
2022-12-27 22:09:55 +0700

    Update 000.c

--- C - TITV/000.c
+++ C - TITV/000.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(){
-    printf("Hello world");//I use git to upload code to github
+    printf("Hello world");//I use git to upload code to github//I don't need you to change my code
     return 0;
 }
